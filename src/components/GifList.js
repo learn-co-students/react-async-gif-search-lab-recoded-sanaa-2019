cf2ab9e043396ba71b5ba11c4faeeb1525e3397e
Atheeralaghbari <atheeralaghbari61@gmail.com>
2019-12-08 09:47:45 -0800

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -5,11 +5,10 @@ export default class GifList extends Component {
    
     
     render() {
-        const {list}=this.props
         return (
             <div>
                 <ul>
-                    {list.map((el,index)=><li key={index}><img src={el}/></li>)}
+                    {this.props.list.map((el,index)=><li key={index}><img src={el}/></li>)}
                     </ul>
             </div>
         )
